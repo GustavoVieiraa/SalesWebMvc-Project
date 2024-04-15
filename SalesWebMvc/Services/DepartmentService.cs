344c11a2895995ccba7e733284a49f058700720d
Gustavo Vieira <gustavovieira292016@gmail.com>
2024-04-14 22:04:00 -0300

    SellerFormViewModel and Department select component created

---
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -0,0 +1,22 @@
+﻿using SalesWebMvc.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Services
+{
+    public class DepartmentService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public DepartmentService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAllDepartments()
+        {
+            return _context.Department.OrderBy(departments => departments.Name).ToList();
+        }
+
+    }
+}
